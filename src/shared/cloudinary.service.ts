e196ea24dc5907892a485e3750c10d3adc50df26
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2026-03-10 17:37:12 +0200

    Ahmedianoo | chore/cloudinary-abstraction | chore(cloudinary): created uploadAudio function inside CloudinaryService class

--- src/shared/cloudinary.service.ts
+++ src/shared/cloudinary.service.ts
@@ -10,6 +10,27 @@ export class CloudinaryService {
       overwrite: false,
     };
     const result = await cloudinary.uploader.upload(filePath, options);
-    return result.secure_url;
+    return {
+      url: result.secure_url,
+      publicId: result.public_id,
+    };
+  }
+
+  static async uploadAudio(filePath: string) {
+    const options = {
+      folder: 'audios',
+      resourse_type: 'video',
+      use_filename: true,
+      unique_filename: true,
+      overwrite: false,
+      format: 'mp3',
+    };
+    const result = await cloudinary.uploader.upload(filePath, options);
+
+    return {
+      url: result.secure_url,
+      duration: result.audio_duration,
+      publicId: result.public_id,
+    };
   }
 }
