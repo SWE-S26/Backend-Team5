c14a7814d70789800fe479738d16315482d6f19d
engseif <eng.seifmohamed2004@gmail.com>
2026-03-10 23:17:19 +0200

    engseif | features/EmailService | engseif | features/EmailService | feat(email): add send password reset link with templates

--- src/shared/abstractions/email/EmailService.ts
+++ src/shared/abstractions/email/EmailService.ts
@@ -1,6 +1,7 @@
 import nodemailer, { Transporter } from 'nodemailer';
 import 'dotenv/config';
-import { tr } from 'zod/v4/locales';
+import fs from 'fs';
+import path from 'path';
 
 class EmailService {
   private transporter: Transporter;
@@ -20,6 +21,43 @@ class EmailService {
   async isEmailServiceWorking() {
     try {
       await this.transporter.verify();
+      return true;
+    } catch (error) {
+      console.log('Email Service Error: ', error);
+    }
+  }
+
+  async sendResetPassowordLink(username: string, userEmail: string) {
+    try {
+      // get paths of html files and image assets
+      const filePath = path.join(
+        __dirname,
+        './templates/requestPasswordReset.html',
+      );
+      const imagePath = path.join(__dirname, './templates/assets/beatza.png');
+      let htmlContent = fs.readFileSync(filePath, { encoding: 'utf-8' });
+
+      // replace in html file with proper params
+      htmlContent = htmlContent.replace('[User]', username);
+      htmlContent = htmlContent.replace(
+        '[resetLink]',
+        'https://beatza-swagger.netlify.app/',
+      );
+
+      // assuming email is correct
+      const info = await this.transporter.sendMail({
+        from: `Beatza Team <${process.env.NODE_MAILER_USER}>`,
+        to: userEmail,
+        subject: `Request to change ${username}'s BeatZa password`,
+        html: htmlContent,
+        attachments: [
+          {
+            filename: 'beatza.png',
+            path: imagePath,
+            cid: 'beatza-logo',
+          },
+        ],
+      });
       return true;
     } catch (error) {
       console.log('Email Service Error: ', error);
