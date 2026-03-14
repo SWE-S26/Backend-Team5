97a425f0405801ae5bcd4545767775c8f977cf1f
engseif <eng.seifmohamed2004@gmail.com>
2026-03-14 23:43:03 +0200

    engseif | features/auth | feat(auth) : added auth endpoint to the collection of routes

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ import integrationRouter from './modules/integration/router.integration';
 import { errorHandler } from './shared/errors/errorHandlerMiddleware';
 import invalidRouter from './shared/errors/router.invalid';
 import logger from './shared/logger/logger';
+import authRouter from './modules/auth/auth.routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(pinoHttp({ logger }));
 app.use('/api', integrationRouter);
+app.use('/auth', authRouter);
 app.use(invalidRouter);
 app.use(errorHandler);
 
