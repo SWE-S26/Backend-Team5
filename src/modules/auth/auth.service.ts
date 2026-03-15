d80eaa1fcb53f7e53f5c6571a1eb2670fb7760fc
engseif <eng.seifmohamed2004@gmail.com>
2026-03-15 04:04:36 +0200

    engseif | features/auth | fix(auth) : Fixed role error in payload JWT

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -37,7 +37,7 @@ export class AuthService {
     const payload = {
       id: user._id,
       email: user.email,
-      role: user.password,
+      role: user.role,
     };
 
     const token = jwt.sign(payload, JWT_SECRET!, { expiresIn: JWT_EXPIRES_IN });
