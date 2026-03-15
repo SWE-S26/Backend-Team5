f1167f257cb03f555f83eca6620d16abf3de5f26
engseif <eng.seifmohamed2004@gmail.com>
2026-03-15 04:10:31 +0200

    engseif | features/auth | feat(auth) : Finished The RequireAuth MiddleWare

--- src/shared/middleware/requireAuth.ts
+++ src/shared/middleware/requireAuth.ts
@@ -5,27 +5,31 @@ import jwt from 'jsonwebtoken';
 import { UnauthorizedError } from '../errors/responseErrors';
 
 function isCross(req: Request): boolean {
-    const userAgent = req.headers['user-agent'] || '';
-    // check if user Agent contains these
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      userAgent,
-    );
+  const userAgent = req.headers['user-agent'] || '';
+  // check if user Agent contains these
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    userAgent,
+  );
 }
 
-export const requireAuth = (req: Request, res: Response, next: NextFunction) {
-    let acessToken ="" 
-    if (isCross(req)) {
-        acessToken = req.body.accessToken
+export const requireAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  let acessToken = '';
+  if (isCross(req)) {
+    acessToken = req.body.accessToken;
+  } else {
+    acessToken = req.cookies['acessToken'];
+  }
+  jwt.verify(acessToken, process.env.JWT_SECRET!, (err, payload) => {
+    if (err) {
+      throw UnauthorizedError('Unauthorized Access');
     } else {
-        acessToken = req.cookies['acessToken']
+      // if req.body it will be overwritten info is cleared
+      (req as any).userInfo = payload;
+      next();
     }
-    jwt.verify(acessToken, process.env.JWT_SECRET!, (err, payload) => {
-        if (err) {
-            throw UnauthorizedError("Unauthorized Access")
-        } else {
-            // if req.body it will be overwritten info is cleared
-            (req as any).userInfo = payload
-            next()
-        }
-    })
-}
+  });
+};
