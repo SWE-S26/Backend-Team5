21e4b2e5196b71f9433bda7af96ed35d5df647bb
engseif <eng.seifmohamed2004@gmail.com>
2026-03-16 22:57:56 +0200

    engseif | chore/publito | chore(publitio) : Made Class Only methods Needed For Each Request

--- src/shared/abstractions/publitio.ts
+++ src/shared/abstractions/publitio.ts
@@ -1,1 +1,36 @@
-//TODO: Make the publitio abstraction to handle file uploads and deletions, and use it in the controllers that need it
+import { initializeConfig } from '../../config/initializeConfig';
+import crypto from 'crypto';
+
+class PublitioMediaStorage {
+  private readonly PUBLITO_KEY = process.env.PUBLITO_KEY;
+  private readonly PUBLITO_SECRET = process.env.PUBLITO_SECRET;
+
+  private generateAPINonce() {
+    const MIN = 10000000;
+    const MAX = 99999999;
+    const publitioNonce = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
+    return publitioNonce.toString();
+  }
+
+  private generateUNIXTimestamp() {
+    return Math.floor(Date.now() / 1000).toString();
+  }
+
+  private generateAPISignature(publitioNonce: string, unixTimestamp: string) {
+    const encrypt = publitioNonce + unixTimestamp + this.PUBLITO_SECRET;
+    const signature = crypto.createHash('sha1').update(encrypt).digest('hex');
+    return signature;
+  }
+
+  private generateReqAuthHeaders() {
+    const nonce = this.generateAPINonce();
+    const timestamp = this.generateUNIXTimestamp();
+    const signature = this.generateAPISignature(nonce, timestamp);
+    return {
+      api_key: this.PUBLITO_KEY,
+      api_nonce: nonce,
+      api_timestamp: timestamp,
+      api_signature: signature,
+    };
+  }
+}
