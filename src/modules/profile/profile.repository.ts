92a5de4bb6847ba46ea1fd01da9e2b784c9b6ea8
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2026-03-17 15:09:33 +0200

    Ahmedianoo | feature/profile-management-module | feat(profile & settings): created repository methods

--- src/modules/profile/profile.repository.ts
+++ src/modules/profile/profile.repository.ts
@@ -1,26 +1,14 @@
+import User, { IUser } from '../../shared/models/models.user';
+
 export class ProfileRepository {
-  async findAll(): Promise<any[]> {
-    // TODO: query your data source
-    return [];
+  async findById(id: string): Promise<IUser | null> {
+    return await User.findById(id).lean();
   }
 
-  async findById(id: string): Promise<any | null> {
-    // TODO: query your data source
-    return null;
-  }
-
-  async create(data: any): Promise<any> {
-    // TODO: insert into your data source
-    return data;
-  }
-
-  async update(id: string, data: any): Promise<any | null> {
-    // TODO: update in your data source
-    return null;
-  }
-
-  async delete(id: string): Promise<boolean> {
-    // TODO: delete from your data source
-    return false;
+  async update(id: string, data: any): Promise<IUser | null> {
+    return await User.findByIdAndUpdate(id, data, {
+      new: true,
+      runValidators: true,
+    }).lean();
   }
 }
