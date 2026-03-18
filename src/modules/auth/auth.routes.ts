d1bccfda715859bff9d49c6dc06f04d5c8d2cad5
ellethy-omar <goosemugger@gmail.com>
2026-03-18 21:00:00 +0200

    ellethy-omar | features/google-oauth | OAuth Hopefully done

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -27,6 +27,11 @@ authRouter.get(
 );
 
 authRouter.post(
+  apiVersions.v1 + '/forgot-password',
+  authController.forgotPassword.bind(authController),
+);
+
+authRouter.post(
   apiVersions.v1 + '/reset-password',
   authController.resetPassword.bind(authController),
 );
