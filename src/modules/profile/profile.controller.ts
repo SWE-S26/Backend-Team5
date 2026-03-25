7f36df160ef8ea5d0b969d6bf6aa1410ddad3e47
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2026-03-25 14:35:17 +0200

    Ahmedianoo | feature/profile-management-module | refactor(profile): refactored error handling in controller

--- src/modules/profile/profile.controller.ts
+++ src/modules/profile/profile.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { parseRequest } from '../../shared/dtos/requestParser';
+import { NotFoundError } from '../../shared/errors/responseErrors';
 import { ProfileService } from './profile.service';
 import { ProfileIdParamDTO } from './dtos/profile.request.params';
 import {
@@ -12,130 +13,87 @@ export class ProfileController {
   constructor(private readonly service: ProfileService) {}
 
   async getProfile(req: Request, res: Response): Promise<void> {
-    try {
-      const validatedRequest = parseRequest(ProfileIdParamDTO, req);
-      if (!validatedRequest.success) {
-        throw validatedRequest.error;
-      }
-
-      const userId = validatedRequest.data!.params.id;
-      const profile = await this.service.getProfile(userId);
-
-      if (!profile) {
-        res.status(404).json({ message: 'User not found' });
-        return;
-      }
-
-      res.json(profile);
-    } catch (err) {
-      res.status(500).json({ message: 'Server error', error: err });
+    const validatedRequest = parseRequest(ProfileIdParamDTO, req);
+    if (!validatedRequest.success) {
+      throw validatedRequest.error;
     }
+
+    const userId = validatedRequest.data!.params.id;
+    const profile = await this.service.getProfile(userId);
+
+    if (!profile) NotFoundError('User not found');
+
+    res.json(profile);
   }
 
   async updateProfile(req: Request, res: Response): Promise<void> {
-    try {
-      const userId = req.userInfo!._id;
-      const validatedRequest = parseRequest(UpdateProfileRequestDTO, req);
+    const userId = req.userInfo!._id;
+    const validatedRequest = parseRequest(UpdateProfileRequestDTO, req);
 
-      if (!validatedRequest.success) {
-        throw validatedRequest.error;
-      }
-
-      const updated = await this.service.updateProfile(
-        userId,
-        validatedRequest.data.body,
-      );
-
-      if (!updated) {
-        res.status(404).json({ message: 'User not found' });
-        return;
-      }
-
-      res.json(updated);
-    } catch (err) {
-      res.status(500).json({ message: 'Server error', error: err });
+    if (!validatedRequest.success) {
+      throw validatedRequest.error;
     }
+
+    const updated = await this.service.updateProfile(
+      userId,
+      validatedRequest.data.body,
+    );
+
+    if (!updated) NotFoundError('User not found');
+
+    res.json(updated);
   }
 
   async updateProfileImages(req: Request, res: Response): Promise<void> {
-    try {
-      const userId = req.userInfo!._id;
+    const userId = req.userInfo!._id;
 
-      const files = req.files as {
-        profileImg?: Express.Multer.File[];
-        bannerImg?: Express.Multer.File[];
-      };
+    const files = req.files as {
+      profileImg?: Express.Multer.File[];
+      bannerImg?: Express.Multer.File[];
+    };
 
-      const validatedRequest = parseRequest(UpdateProfileImagesRequestDTO, req);
+    const validatedRequest = parseRequest(UpdateProfileImagesRequestDTO, req);
 
-      if (!validatedRequest.success) {
-        throw validatedRequest.error;
-      }
-      const flags = validatedRequest.data.body;
+    if (!validatedRequest.success) throw validatedRequest.error;
 
-      const updated = await this.service.updateProfileImages(
-        userId,
-        flags,
-        files,
-      );
+    const flags = validatedRequest.data.body;
 
-      if (!updated) {
-        res.status(404).json({ message: 'User not found' });
-        return;
-      }
+    const updated = await this.service.updateProfileImages(
+      userId,
+      flags,
+      files,
+    );
 
-      res.json(updated);
-    } catch (err) {
-      res.status(500).json({ message: 'Server error', error: err });
+    if (!updated) {
+      NotFoundError('User not found');
     }
+
+    res.json(updated);
   }
 
   async getPrivacySettings(req: Request, res: Response): Promise<void> {
-    try {
-      console.log('userId================================================');
-      const userId = req.userInfo!._id;
-      console.log(
-        'userId================================================',
-        userId,
-      );
-      const settings = await this.service.getPrivacySettings(userId);
-      console.log(
-        'settings================================================',
-        settings,
-      );
-      if (!settings) {
-        res.status(404).json({ message: 'User settings not found' });
-        return;
-      }
-      res.json(settings);
-    } catch (err) {
-      res.status(500).json({ message: 'Server error', error: err });
-    }
+    const userId = req.userInfo!._id;
+
+    const settings = await this.service.getPrivacySettings(userId);
+    if (!settings) NotFoundError('User settings not found');
+
+    res.json(settings);
   }
 
   async updatePrivacySettings(req: Request, res: Response): Promise<void> {
-    try {
-      const userId = req.userInfo!._id;
-      const validatedRequest = parseRequest(
-        UpdatePrivacySettingsRequestDTO,
-        req,
-      );
+    const userId = req.userInfo!._id;
+    const validatedRequest = parseRequest(UpdatePrivacySettingsRequestDTO, req);
 
-      if (!validatedRequest.success) {
-        throw validatedRequest.error;
-      }
-
-      const updated = await this.service.updatePrivacySettings(
-        userId,
-        validatedRequest.data.body,
-      );
-      if (!updated) {
-        res.status(404).json({ message: 'User settings not found' });
-        return;
-      }
-      res.json(updated);
-    } catch (err) {
-      res.status(500).json({ message: 'Server error', error: err });
+    if (!validatedRequest.success) {
+      throw validatedRequest.error;
     }
+
+    const updated = await this.service.updatePrivacySettings(
+      userId,
+      validatedRequest.data.body,
+    );
+    if (!updated) NotFoundError('User settings not found');
+
+    res.json(updated);
   }
 }
