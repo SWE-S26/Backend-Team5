23c2f1375e50c41f893370c3571d229923a72841
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2026-03-26 21:14:25 +0200

    Ahmedianoo | feature/profile-management-module | fix(user): updated role types in user

--- src/shared/models/models.user.ts
+++ src/shared/models/models.user.ts
@@ -8,7 +8,7 @@ export type IUser = {
   email: string;
   password: string;
   googleId?: string;
-  role: 'Listener/Artist' | 'Admin';
+  role: 'Listener' | 'Artist' | 'Pro' | 'Admin';
   displayName: string;
   firstName: string;
   lastName: string;
