1aaa30ffb0cabd40430f285643550bc52e4d7ff2
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2026-04-02 01:42:48 +0200

    Ahmedianoo | feature/followers-and-social-graphs | feat(social): added get followers and followed endpoints

--- src/modules/following/following.routes.ts
+++ src/modules/following/following.routes.ts
@@ -20,4 +20,14 @@ followingRouter.post(
   followingController.removeFollower.bind(followingController),
 );
 
+followingRouter.get(
+  apiVersions.v1 + '/followers/:id',
+  followingController.getFollowers.bind(followingController),
+);
+
+followingRouter.get(
+  apiVersions.v1 + '/following/:id',
+  followingController.getFollowed.bind(followingController),
+);
+
 export default followingRouter;
