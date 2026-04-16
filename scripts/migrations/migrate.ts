a3453d3dd8644ab84d73dbbc65eab27805cebf97
ellethy-omar <goosemugger@gmail.com>
2026-04-16 19:11:59 +0200

    ellethy-omar | refactor/auth | Migrations done

--- scripts/migrations/migrate.ts
+++ scripts/migrations/migrate.ts
@@ -1,17 +1,17 @@
 import { initializeDbConnection } from '../../src/config/db/connect';
 import mongoose, { mongo } from 'mongoose';
 import logger from '../../src/shared/logger/logger';
-import User from '../../src/shared/models/models.user';
-import Settings from '../../src/shared/models/models.settings';
-import { cleanUpDeletedUsers } from './cleanUpDeletedUsers';
-import { deleteStaleUnverifiedUsers } from './cleanUpUnverified';
-import { cleanUpTracks } from './cleanupTracks';
+import { cleanUpDeletedUsers } from './cleanUpDeletedUsersSettings';
+import { deleteStaleUnverifiedUsers } from './cleanUpUnverifiedUsers';
+import { changeTracksImages } from './changeAudioImages';
+import { changeProfileImages } from './changeProfileImages';
 
 const runMigrations = async () => {
   try {
-    // await cleanUpDeletedUsers();
-    // await deleteStaleUnverifiedUsers();
-    await cleanUpTracks();
+    await cleanUpDeletedUsers();
+    await deleteStaleUnverifiedUsers();
+    await changeTracksImages();
+    await changeProfileImages();
   } catch (e) {
     if (e instanceof Error) {
       logger.error(`Error during migration execution: ${e.message}`);
@@ -34,5 +34,4 @@ initializeDbConnection()
   })
   .finally(async () => {
     await mongoose.connection.close();
-    logger.info('[MongoDB] Connection closed.');
   });
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,18 +1,25 @@
 export const DEFAULT_PROFILE_IMAGE = {
   imgLink:
+    'https://res.cloudinary.com/dr1sqiouu/image/upload/v1776357570/profile/file_ud25rn.png',
+  publicId: 'profile/file_ud25rn',
+};
+
+export const OLD_DEFAULT_PROFILE_IMAGE = {
+  imgLink:
     'https://res.cloudinary.com/dexluedse/image/upload/v1744719629/mobile-app/lwvswk21xn3wpgoufqxi.jpg',
   publicId: 'mobile-app/lwvswk21xn3wpgoufqxi',
 };
 
 export const DEFAULT_AUDIO_IMAGE = {
   imgLink:
-    'https://res.cloudinary.com/dr1sqiouu/image/upload/v1774613682/audio/file_d8zkpy.png',
-  publicId: 'audio/file_d8zkpy',
+    'https://res.cloudinary.com/dr1sqiouu/image/upload/v1776357591/audio/file_scswac.png',
+  publicId: 'audio/file_scswac',
 };
 
-export const DEFAULT_FAMILY_FRIENDLY_IMAGE = {
-  url: 'https://res.cloudinary.com/dr1sqiouu/image/upload/v1776194326/audio/file_wfcupp.jpg',
-  publicId: 'audio/file_wfcupp',
+export const OLD_DEFAULT_AUDIO_IMAGE = {
+  imgLink:
+    'https://res.cloudinary.com/dr1sqiouu/image/upload/v1774613682/audio/file_d8zkpy.png',
+  publicId: 'audio/file_d8zkpy',
 };
 
 export const DEFAULT_PLAYLIST_IMAGE = {
