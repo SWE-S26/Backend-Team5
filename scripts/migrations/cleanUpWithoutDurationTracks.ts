204b3f9dade7cd83e07cb2cede7bef81a94d5aa4
engseif <eng.seifmohamed2004@gmail.com>
2026-04-18 18:01:30 +0200

    engseif | feature/track-management | Tracks with no duration Migration fixed

--- scripts/migrations/cleanUpWithoutDurationTracks.ts
+++ scripts/migrations/cleanUpWithoutDurationTracks.ts
@@ -4,7 +4,7 @@ import Track from '../../src/shared/models/models.track';
 
 export const deleteTracksWithoutDuration = async (): Promise<void> => {
   const result = await Track.deleteMany({
-    durationInSecond: { $exists: false },
+    durationInSeconds: { $exists: false },
   });
 
   logger.info(`Deleted ${result.deletedCount} Tracks with No Duration`);
