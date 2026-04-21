253914a326b622d05c3811e921a18a7573c01278
engseif <eng.seifmohamed2004@gmail.com>
2026-04-22 00:51:08 +0200

    engseif | feature/track-management | quota became 8 daily

--- src/modules/tracks/tracks.routes.ts
+++ src/modules/tracks/tracks.routes.ts
@@ -6,7 +6,7 @@ import multer from 'multer';
 
 const uploadTrackLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000,
-  max: 10,
+  max: 8,
   keyGenerator: (req) => String(req.userInfo!._id),
   message: 'You can only upload one track every 24 hours.',
 });
