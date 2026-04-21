3a5c315fd0add80e4002a53a03339a49145d55db
engseif <eng.seifmohamed2004@gmail.com>
2026-04-22 00:17:16 +0200

    engseif | feature/message | fixed little bug in mapper

--- src/modules/messaging/messaging.service.ts
+++ src/modules/messaging/messaging.service.ts
@@ -353,7 +353,10 @@ export class MessagingService {
       content,
     );
     return {
-      updatedConversation,
+      updatedConversation: MessagingMapper.toChatHistoryReceiverResponse(
+        updatedConversation,
+        userId,
+      ),
       receiverSettings,
     };
   }
