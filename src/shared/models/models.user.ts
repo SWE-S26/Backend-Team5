9510fffae04e2e8bd73c65b4f7e712a481e238c1
engseif <eng.seifmohamed2004@gmail.com>
2026-04-22 17:36:39 +0200

    engseif | feature/track-management | fixed errors related to updated publitio interface

--- src/shared/models/models.user.ts
+++ src/shared/models/models.user.ts
@@ -439,7 +439,7 @@ userSchema.post('findOneAndDelete', async function (doc: IUser | null) {
     await Promise.allSettled(
       userTracks.map((track) => {
         publitioMediaStorage
-          .deleteAudioTrack(track.audio.id)
+          .deleteAudioTrack(track.audio.id, track.audio.cloudIndex)
           .then(() => {
             logger.debug(
               `Successfully deleted audio track from Publitio for user ${doc._id}`,
