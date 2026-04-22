9cb4ebd15c6ed047a10d36be6e79f063dcbf83ee
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2026-04-22 10:03:06 +0200

    Ahmedianoo | feat/fead-search-dtos | feat(dtos): created dtos for response

--- src/modules/feed/dtos/feed.response.ts
+++ src/modules/feed/dtos/feed.response.ts
@@ -1,17 +1,70 @@
+import z from 'zod';
 import extendedZod from '../../../shared/docs/dtoDocumenter';
 
-export const FeedResponseDto = extendedZod
-  .object({
-    id: extendedZod.string(),
-    email: extendedZod.string(),
-    name: extendedZod.string(),
-    role: extendedZod.enum(['user', 'admin']),
-  })
-  .openapi('FeedResponse', {
-    example: {
-      id: '697b7c75001e8cb1d4c0bb67',
-      email: 'user@example.com',
-      name: 'cow',
-      role: 'user',
-    },
-  });
+export const FeedItemDTO = extendedZod.object({
+  id: extendedZod.mongoId(),
+  type: extendedZod.enum(['track', 'playlist', 'album']),
+  isRepost: extendedZod.boolean(),
+  createdAt: extendedZod.string(),
+  actorId: extendedZod.mongoId(),
+  displayName: extendedZod.string(),
+  profileImg: extendedZod.string().url().nullable(),
+});
+
+export const FeedResponseDTO = extendedZod.object({
+  results: extendedZod.array(FeedItemDTO),
+  hasMore: extendedZod.boolean(),
+});
+
+export type FeedResponseDTOType = z.infer<typeof FeedResponseDTO>;
+
+export const SearchResultDTO = extendedZod.object({
+  id: extendedZod.mongoId(),
+  type: extendedZod.enum(['track', 'user', 'playlist', 'album']),
+});
+
+export const SearchCountsDTO = extendedZod.object({
+  tracks: extendedZod.number(),
+  users: extendedZod.number(),
+  playlists: extendedZod.number(),
+  albums: extendedZod.number(),
+});
+
+export const SearchResponseDTO = extendedZod.object({
+  counts: SearchCountsDTO,
+  results: extendedZod.array(SearchResultDTO),
+  hasMore: extendedZod.boolean(),
+});
+
+export type SearchResponseDTOType = z.infer<typeof SearchResponseDTO>;
+
+export const SearchSuggestionDTO = extendedZod.object({
+  id: extendedZod.mongoId(),
+  type: extendedZod.enum(['track', 'user', 'playlist', 'album']),
+  title: extendedZod.string(),
+  imgLink: extendedZod.string().url().nullable(),
+  isPersonalized: extendedZod.boolean(),
+});
+
+export type SearchSuggestionDTOType = z.infer<typeof SearchSuggestionDTO>;
+
+export const SearchHistoryItemDTO = extendedZod.object({
+  id: extendedZod.mongoId(),
+  type: extendedZod.enum(['track', 'user', 'playlist', 'album']),
+  title: extendedZod.string(),
+  imageUrl: extendedZod.string().url().nullable(),
+  metadata: extendedZod.string().nullable(),
+});
+
+export type SearchHistoryItemDTOType = z.infer<typeof SearchHistoryItemDTO>;
+
+export const TrendingTrackDTO = extendedZod.object({
+  id: extendedZod.mongoId(),
+});
+
+export const TrendingResponseDTO = extendedZod.object({
+  results: extendedZod.array(TrendingTrackDTO),
+  hasMore: extendedZod.boolean(),
+});
+
+export type TrendingResponseDTOType = z.infer<typeof TrendingResponseDTO>;
