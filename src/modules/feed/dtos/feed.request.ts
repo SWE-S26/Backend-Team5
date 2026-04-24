df24a5a05c2b37c7b54773058a0381a8fb292b23
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2026-04-24 14:38:49 +0300

    Ahmedianoo | feat/feed-search | fix(feed): updated dtos and docs of trending feed to accept userId

--- src/modules/feed/dtos/feed.request.ts
+++ src/modules/feed/dtos/feed.request.ts
@@ -6,8 +6,14 @@ import {
 } from './feed.request.query';
 import { HistoryIdParamDTO } from './feed.request.params';
 import { AddToHistoryBodyDTO } from './feed.request.body';
+import { idParamDto } from '../../../shared/dtos/commonDTO';
 
 export const GetFeedRequestDTO = extendedZod.object({
+  query: FeedQueryDTO,
+});
+
+export const GetTrendingTracksRequestDTO = extendedZod.object({
+  params: idParamDto,
   query: FeedQueryDTO,
 });
 
