f64dc55c1180a8df7190080ab8419c815d977570
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2026-04-25 15:47:59 +0300

    Ahmedianoo | fix/pagination-type-in-zod-social-feed | fix(pagination): fixed limit and offset types

--- src/modules/following/dtos/following.request.query.ts
+++ src/modules/following/dtos/following.request.query.ts
@@ -2,6 +2,6 @@ import extendedZod from '../../../shared/docs/dtoDocumenter';
 import { PaginationQueryDto } from '../../../shared/dtos/commonDTO';
 
 export const PaginationQueryDTO = extendedZod.object({
-  offset: extendedZod.number().min(0).optional().default(0),
-  limit: extendedZod.number().min(1).max(50).optional().default(20),
+  offset: extendedZod.coerce.number().min(0).optional().default(0),
+  limit: extendedZod.coerce.number().min(1).max(50).optional().default(20),
 });
