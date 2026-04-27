0cd613ea7283197a3da78e2ff7854329f2c2af52
engseif <eng.seifmohamed2004@gmail.com>
2026-04-28 01:01:10 +0300

    engseif | feature/track-management | Added AudioClip Start and End To Response

--- src/modules/tracks/dtos/tracks.mapper.ts
+++ src/modules/tracks/dtos/tracks.mapper.ts
@@ -92,6 +92,10 @@ export class TracksMapper {
       isLikedByUser: track.likedBy.map((id) => id.toString()).includes(userId),
       permissions: track.permissions,
       license: track.license,
+      audioClip: {
+        start: track.audioClip?.start ?? 0,
+        end: track.audioClip?.end ?? 0,
+      },
     };
   }
 
@@ -119,6 +123,10 @@ export class TracksMapper {
       releaseDate: track.createdAt,
       permissions: track.permissions,
       license: track.license,
+      audioClip: {
+        start: track.audioClip?.start ?? 0,
+        end: track.audioClip?.end ?? 0,
+      },
     };
   }
 
