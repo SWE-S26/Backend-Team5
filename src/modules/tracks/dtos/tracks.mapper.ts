4c6b98a91b5a0fe912de1ba5dcfd6efcbb91aceb
engseif <eng.seifmohamed2004@gmail.com>
2026-04-28 00:35:10 +0300

    engseif | feature/track-management | Fixed internal Logic - Added Playlists And Albums Including track - Docs Done

--- src/modules/tracks/dtos/tracks.mapper.ts
+++ src/modules/tracks/dtos/tracks.mapper.ts
@@ -107,6 +107,8 @@ export class TracksMapper {
       numComments: track.comments.length,
       releaseDate: track.createdAt,
       isLikedByUser: track.likedBy.map((id) => id.toString()).includes(userId),
+      permissions: track.permissions,
+      license: track.license,
     };
   }
 
@@ -149,6 +151,8 @@ export class TracksMapper {
       numReposts: track.numberOfReposts,
       numComments: track.comments.length,
       releaseDate: track.createdAt,
+      permissions: track.permissions,
+      license: track.license,
     };
   }
 
