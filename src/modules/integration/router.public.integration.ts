d4e1a621178a8547fddc035c5a57860756a68e70
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2026-04-29 00:21:10 +0300

    Ahmedianoo | feature/add-block-flag-to-lists | feat(profile): added public profile endpoints

--- src/modules/integration/router.public.integration.ts
+++ src/modules/integration/router.public.integration.ts
@@ -3,6 +3,7 @@ import { engagementPublicRouter } from '../engagement/engagement.routes';
 import { paymentPublicRouter } from '../payment/payment.routes';
 import { tracksPublicRouter } from '../tracks/tracks.routes';
 import { playlistsPublicRouter } from '../playlists/playlists.routes';
+import { profilePublicRouter } from '../profile/profile.routes';
 
 const publicIntegrationRouter = Router();
 
@@ -10,5 +11,6 @@ publicIntegrationRouter.use('/engagement', engagementPublicRouter);
 publicIntegrationRouter.use('/payment', paymentPublicRouter);
 publicIntegrationRouter.use('/tracks', tracksPublicRouter);
 publicIntegrationRouter.use('/playlists', playlistsPublicRouter);
+publicIntegrationRouter.use('/profile', profilePublicRouter);
 
 export default publicIntegrationRouter;
