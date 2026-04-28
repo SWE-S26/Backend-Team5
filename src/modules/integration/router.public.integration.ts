59a3d0b218249365728808f669dccb30f582466c
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2026-04-29 02:13:35 +0300

    Ahmedianoo | feature/add-block-flag-to-lists | feat(feed): modified trending tracks endpoint to be public

--- src/modules/integration/router.public.integration.ts
+++ src/modules/integration/router.public.integration.ts
@@ -5,6 +5,7 @@ import { tracksPublicRouter } from '../tracks/tracks.routes';
 import { playlistsPublicRouter } from '../playlists/playlists.routes';
 import { profilePublicRouter } from '../profile/profile.routes';
 import { followingPublicRouter } from '../following/following.routes';
+import { feedPublicRouter } from '../feed/feed.routes';
 
 const publicIntegrationRouter = Router();
 
@@ -14,5 +15,6 @@ publicIntegrationRouter.use('/tracks', tracksPublicRouter);
 publicIntegrationRouter.use('/playlists', playlistsPublicRouter);
 publicIntegrationRouter.use('/profile', profilePublicRouter);
 publicIntegrationRouter.use('/social', followingPublicRouter);
+publicIntegrationRouter.use('/feed', feedPublicRouter);
 
 export default publicIntegrationRouter;
